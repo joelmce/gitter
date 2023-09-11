52b05e1af2e669d1d6424b42e5f2c9007ae6d26d
Joel McElroy <joelanthony.mac@gmail.com>
2023-09-11 10:18:27 +1000

    listen

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const pullRequest = require("./api/pullrequest.js");
 app.use(express.json());
 
 app.use("/api/pr", pullRequest);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
